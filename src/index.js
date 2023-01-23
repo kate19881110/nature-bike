51b1e6f05b43eebcc923f22cf603908fa4a70247
KateLiu <KateLiu@wistronits.com>
2023-01-23 22:42:42 +0800

    add router and login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Login from "../src/containers/Login/index";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
