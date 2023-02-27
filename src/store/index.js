67410af5c6b450072db3d856f592e378703c5a40
KateLiu <KateLiu@wistronits.com>
2023-02-27 09:49:24 +0800

    add useContext and modify  click value

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+ const Context = React.createContext({
+  name: "123",
+  authorityName: "",
+  city: "",
+  cyclingLength: 0,
+  start: "",
+  end: "",
+});
+
+export default Context;
